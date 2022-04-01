0c731d485aeaacfd86cedb3b18b9717b36c5ce95
Logan Nguyen <Lgad31vn@gmail.com>
2022-04-01 18:34:43 -0400

    Added javadocs to IOService, cleaned up services

--- yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/services/IOService.java
+++ yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/services/IOService.java
@@ -1,5 +1,6 @@
 package yelp.dataset.oswego.yelpbackend.services;
 
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,6 +9,8 @@ import java.io.ObjectOutputStream;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import lombok.NoArgsConstructor;
 import yelp.dataset.oswego.yelpbackend.dataStructure.btree.BusinessBtree;
@@ -15,8 +18,13 @@ import yelp.dataset.oswego.yelpbackend.dataStructure.btree.BusinessBtree;
 @NoArgsConstructor
 public class IOService {
     private final String bTree = System.getProperty("user.dir") + "/yelp-app/yelp-dataset/btree.bin";
-    private final String bTreeRoot = System.getProperty("user.dir") + "/yelp-app/yelp-dataset/btree-root.bin";
+    private final String bTreeRoot = System.getProperty("user.dir") + "/yelp-app/yelp-dataset/btree-node.bin";
 
+    /**
+     * A function to write a whole Btree to disk
+     * @param businessBtree
+     * @throws IOException
+     */
     protected void writeBtree(BusinessBtree businessBtree) throws IOException {
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(bTree));
         oos.writeObject(businessBtree);
@@ -24,6 +32,11 @@ public class IOService {
         oos.close();
     }
 
+    /**
+     * A function to read the B-tree from disk
+     * @return BusinessBtree
+     * @throws IOException
+     */
     protected BusinessBtree readBtree() throws IOException {
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream(bTree));
         try {
@@ -37,10 +50,15 @@ public class IOService {
         }
     }
 
-
+    /**
+     * A function to write a btree node to disk using File Channel and Buffer
+     * @param businessBtree
+     * @throws IOException
+     */
     protected void writeNode(BusinessBtree businessBtree) throws IOException {
         // Get the file
         RandomAccessFile raf = new RandomAccessFile(bTreeRoot, "rw");
+        raf.seek(0);
         // Register FileChannel to operate read and write
         FileChannel wChannel = raf.getChannel();
 
@@ -48,51 +66,68 @@ public class IOService {
         ByteBuffer wBuffer = ByteBuffer.allocate(4096); // 4KB
 
         // Write information to wBuffer
-        wBuffer.put("writing node...".getBytes());
-         
-        
+        wBuffer.put("writing to disk...".getBytes());
+
         // ByteBuffer::flip() is used to flip BB from "reading from I/O"(put) to "writing to I/O"(get) after a sequence of put
         wBuffer.flip();
 
         // Acctually write to file from buffer
         wChannel.write(wBuffer);
 
+        // forcely flushes all unwritten data from channel to disk
+        wChannel.force(true);
+
         // clean up
         wBuffer.clear();
         wChannel.close();
         raf.close();
         
-        System.out.println("Successfully writing node to file!");
+        System.out.println("Successfully wrote node to file!");
     }
 
+    /**
+     * A function to read a btree node from disk
+     * @throws IOException
+     */
     protected void readNode() throws IOException {
         // Get the file
         RandomAccessFile raf = new RandomAccessFile(bTreeRoot, "rw");
 
         // Register FileChannel to operate read and write
-        FileChannel wChannel = raf.getChannel();
+        FileChannel rChannel = raf.getChannel();
 
-        // Allocaate wFufferSize 
-        int wFufferSize = 4096; // 4KB
-        if (wFufferSize > wChannel.size()) {
-            wFufferSize = (int) wChannel.size();
+        // Allocaate rBufferSize 
+        int rBufferSize = 4096; // 4KB
+        if (rBufferSize > rChannel.size()) {
+            rBufferSize = (int) rChannel.size();
         }
         
         // Register ByteBuffer to store message inside a buffer
-        ByteBuffer wBuffer = ByteBuffer.allocate(wFufferSize);
+        ByteBuffer rBuffer = ByteBuffer.allocate(rBufferSize);
 
         // Read the file to buffer
-        wChannel.read(wBuffer);
+        rChannel.read(rBuffer);
         
-        // Flip the wBufferer
-        wBuffer.flip();
+        // Flip the rBufferer
+        rBuffer.flip();
 
         // clean up
-        wBuffer.clear();
-        wChannel.close();
+        rBuffer.clear();
+        rChannel.close();
         raf.close();
         
-        System.out.println("Reading from wBuffer: " +new String(wBuffer.array()));
+        System.out.println("Reading from wBuffer: " +new String(rBuffer.array()));
+    }
+
+    /**
+     * A function to empty a file
+     * @param PATH
+     * @throws IOException
+     */
+    public void emptyFile(String PATH) throws IOException{
+        BufferedWriter writer = Files.newBufferedWriter(Paths.get(PATH));
+        writer.write("");
+        writer.flush();
     }
 
 }
--- yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/services/RunnerService.java
+++ yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/services/RunnerService.java
@@ -4,21 +4,11 @@ import java.util.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import yelp.dataset.oswego.yelpbackend.algorithms.clustering.Centroid;
-import yelp.dataset.oswego.yelpbackend.algorithms.clustering.KMeans;
-import yelp.dataset.oswego.yelpbackend.dataStructure.btree.BusinessBtree;
-import yelp.dataset.oswego.yelpbackend.models.BusinessModel;
-
 @Component // this + CommandLineRunner are used to run code at application startup -like useEffect in React
 public class RunnerService implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
         // new IOService().writeBtree(new JsonService().initBusinessBtree("/Users/logan/coding/SUNY_Oswego/CSC-365/In_Class/Assignment2/yelp-app/yelp-dataset/business.json"));
-
-        BusinessBtree businessBtree = new IOService().readBtree();
-        Map<Centroid, List<BusinessModel>> clusters = new KMeans().initializeClusers(businessBtree, 7);
-
-
     }
 }
 
