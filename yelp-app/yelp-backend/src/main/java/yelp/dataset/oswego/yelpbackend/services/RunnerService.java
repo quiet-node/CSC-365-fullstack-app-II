f6a6b9f995b542162d30a43de1af52b4553801aa
Logan Nguyen <Lgad31vn@gmail.com>
2022-04-01 00:52:38 -0400

    Update RunnerService.java

--- yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/services/RunnerService.java
+++ yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/services/RunnerService.java
@@ -4,16 +4,17 @@ import java.util.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import yelp.dataset.oswego.yelpbackend.algorithms.clustering.Centroid;
 import yelp.dataset.oswego.yelpbackend.algorithms.clustering.KMeans;
 import yelp.dataset.oswego.yelpbackend.dataStructure.btree.BusinessBtree;
+import yelp.dataset.oswego.yelpbackend.models.BusinessModel;
 
 @Component // this + CommandLineRunner are used to run code at application startup -like useEffect in React
 public class RunnerService implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
         BusinessBtree businessBtree = new IOService().readBtree();
-
-        new KMeans().initializeClusers(businessBtree, 7);
+        Map<Centroid, List<BusinessModel>> clusters = new KMeans().initializeClusers(businessBtree, 7);
     }
 }
 
